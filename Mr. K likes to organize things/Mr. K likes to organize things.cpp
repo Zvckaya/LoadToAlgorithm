c5bc047b27a7d4450c3b7701aa29afdadc18c098
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-09 23:32:10 +0900

    정리 정돈을 좋아하는 k 씨 완료

---
+++ Mr. K likes to organize things/Mr. K likes to organize things.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < m; i++) {
+        int a, s, d; // 범위 시작, 범위 종료, 선택자 
+        cin >> a >> s >> d;
+
+        // 0-based index로 변환
+        vector<int> copy_arr(arr.begin() + a - 1, arr.begin() + s);
+
+        sort(copy_arr.begin(), copy_arr.end());
+
+        cout << copy_arr[d - 1] << endl; // 0-based index로 접근
+    }
+
+    return 0;
+}
