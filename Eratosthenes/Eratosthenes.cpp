d5212e75f01f84de964a8750cf3b96d381278345
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-09 22:46:47 +0900

    Eratosthenes

--- Eratosthenes/Eratosthenes.cpp
+++ Eratosthenes/Eratosthenes.cpp
@@ -5,11 +5,32 @@ int main()
 {
 	const int N = 100;
 
-	bool is_prime[N] = { false };
+	bool is_prime[N];
 
-	for (auto n : is_prime) {
-		cout << n << "\n";
+	is_prime[0] = false;
+	is_prime[1] = false;
+
+	for (int i = 2;i< 100;i++) {
+		is_prime[i] = true;
 	}
+
+	for (int i = 2; i < N; i++) {
+		if (is_prime) {
+			for (int j = i * 2; j < N; j += j) {
+				is_prime[j] = false;
+			}
+		}
+	}
+
+	for (int i = 2; i <= N; ++i) {
+		if (is_prime[i]) {
+			cout << i << " 소수입니다.\n";
+		}
+		else {
+			cout << i << " 소수가 아닙니다.\n";
+		}
+	}
+
 
 }
 
