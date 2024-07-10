5b90a3c6096e0b61bdd15544d499a2b15d3e6d66
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-10 21:52:18 +0900

    decompressString

---
+++ Divide and Conquer/Divide and Conquer.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int fibo(int N) {
+    if (N <= 2)
+        return 1;
+    return fibo(N - 1) + fibo(N - 2);
+}
+
+int main()
+{
+    cout << fibo(4);
+}
+
+
---
+++ String decompression/String decompression.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int decompressString(string s) {
+    int tmp = 0;
+
+
+    bool isMulti = false;
+
+    for (int i = s.length()-1; i >= 0; i--) {
+        if (isMulti) {
+            tmp = (s[i] - '0') * tmp;
+            isMulti = false;
+        }
+        else {
+            if (isdigit(s[i])) {
+                tmp++;
+            }
+            else if (s[i] == '(') {
+                isMulti = true;
+                continue;
+            }
+        }
+   }
+
+    return tmp;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    cout << decompressString(s) << endl;
+    return 0;
+}
