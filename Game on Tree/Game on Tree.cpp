021273282bf6d6d103b6607c22650a04f29d8c87
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-12 14:58:55 +0900

    game on tree

---
+++ Game on Tree/Game on Tree.cpp
@@ -0,0 +1,137 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Node {
+public:
+    vector<Node*> children;
+    int value;
+    Node(int n) :value (n) {};
+
+    void PrintNode() {
+        if (children.empty())
+            cout << value << "의 자식노드는 없습니다."<<"\n";
+        else {
+            cout << value << "의 자식노드들은";
+            for (auto i : children)
+                cout << i->value;
+            cout << "입니다"<<"\n";
+        }
+    }
+
+    void addChild(Node* child) {
+        children.push_back(child);
+    }
+};
+
+
+int findWinner(vector<Node*> nodes, int start) {
+    int p1 = 0;
+    int p2 = 0;
+    int next = 0;
+
+    bool isP1 = true;
+
+    Node* cursor = nodes[start - 1];
+    
+
+    while (true)
+    {
+        if (cursor->children.empty())
+            break;
+        else {
+            if (isP1) 
+                p1 += cursor->value;
+            else 
+                p2 += cursor->value;
+            isP1 != isP1;
+            
+            for (auto i : cursor->children) {
+                if (i->value > next)
+                    next = i->value;
+            }
+            cursor = nodes[next - 1];
+        }
+
+    }
+
+    if (p1 >= p2)
+        return 1;
+    else
+        return 0;
+}
+
+
+int main() {
+    vector<Node*> nodes;
+
+    int length;
+    cin >> length;
+
+    for (int i = 1; i <= length; i++) {
+        nodes.emplace_back(new Node(i));
+    }
+    
+    for (int i = 0; i < length - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        if (u < v)
+            nodes[u - 1]->addChild(nodes[v - 1]);
+        else 
+            nodes[v - 1]->addChild(nodes[u - 1]);
+        u, v = 0;
+    }
+
+    for (int i = 1; i < length; i++) {
+        cout << findWinner(nodes, i) << "\n";
+    }
+
+
+    for (auto i : nodes) {
+        delete i;
+    }
+
+    /*for (int i = 1; i <= length; i++) {
+        int p1 = 0;
+        int p2 = 0;
+        bool isP1 = true;
+
+        Node cursor = nodes[i-1];
+     
+        
+        while (true)
+        {
+            int max = 0;
+            if (isP1) {
+                p1 += cursor.value;
+                isP1 = false;
+            }
+            else {
+                p2 += cursor.value;
+                isP1 = true;
+            }
+            
+            if (cursor.nodes.empty())
+                break;
+
+
+            for (auto i : cursor.nodes) {
+                if (i.value > max)
+                    max = i.value;
+            }
+            cursor = nodes[max - 1];
+            max = 0;
+        }
+
+        if (p1 >= p2)
+            cout << "1";
+        else
+            cout << "0";
+        p1 = 0;
+        p2 = 0;
+    }
+    */
+
+}
--- String decompression/String decompression.cpp
+++ String decompression/String decompression.cpp
@@ -1,37 +1,89 @@
-﻿#include <iostream>
+﻿//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//
+//int decompressString(string s) {
+//    int tmp = 0;
+//
+//
+//    bool isMulti = false;
+//
+//    for (int i = s.length()-1; i >= 0; i--) {
+//        if (isMulti) {
+//            tmp = (s[i] - '0') * tmp;
+//            isMulti = false;
+//        }
+//        else {
+//            if (isdigit(s[i])) {
+//                tmp++;
+//            }
+//            else if (s[i] == '(') {
+//                isMulti = true;
+//                continue;
+//            }
+//        }
+//   }
+//
+//    return tmp;
+//}
+//
+//int main() {
+//    string s;
+//    cin >> s;
+//    cout << decompressString(s) << endl;
+//    return 0;
+//}
+#include <iostream>
 #include <string>
+#include <stack>
 
 using namespace std;
 
+int decompressLength(const string& s) {
+    stack<int> stackLength;
+    stack<int> stackRepeat;
+    int length = 0;
+    int i = 0;
 
-int decompressString(string s) {
-    int tmp = 0;
+    while (i < s.length()) {
+        char c = s[i];
 
-
-    bool isMulti = false;
-
-    for (int i = s.length()-1; i >= 0; i--) {
-        if (isMulti) {
-            tmp = (s[i] - '0') * tmp;
-            isMulti = false;
+        if (isdigit(c)) {
+            int repeat = c - '0';
+            if (i + 1 < s.length() && s[i + 1] == '(') {
+                stackRepeat.push(repeat);
+                stackLength.push(length);
+                length = 0;
+                i++;
+            }
+            else {
+                length++;
+            }
+        }
+        else if (c == ')') {
+            int repeat = stackRepeat.top();
+            stackRepeat.pop();
+            length = stackLength.top() + repeat * length;
+            stackLength.pop();
         }
         else {
-            if (isdigit(s[i])) {
-                tmp++;
-            }
-            else if (s[i] == '(') {
-                isMulti = true;
-                continue;
-            }
+            length++;
         }
-   }
+        i++;
+    }
 
-    return tmp;
+    return length;
 }
 
 int main() {
-    string s;
-    cin >> s;
-    cout << decompressString(s) << endl;
+    string input;
+    cout << "압축된 문자열을 입력하세요: ";
+    getline(cin, input);
+
+    int result = decompressLength(input);
+    cout << "압축 해제한 문자열의 길이: " << result << endl;
+
     return 0;
 }
