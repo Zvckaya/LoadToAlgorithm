c352689126c0cf3c7d8841c5410a75c16ac4abd1
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-15 20:20:17 +0900

    no10828

---
+++ no10828/no10828.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	stack<int> myStack;
+
+	int N;
+	cin >> N;
+	string str;
+
+	while (N>0)
+	{
+		cin >> str;
+		if (str == "push") {
+			int tmp;
+			cin >> tmp;
+			myStack.push(tmp);
+		}
+		else if (str == "pop") {
+			if (myStack.empty())
+				cout << -1<<"\n";
+			else {
+				cout << myStack.top() << "\n";
+				myStack.pop();
+			}
+		}
+		else if (str == "size") {
+			cout << myStack.size() << "\n";
+		}
+		else if(str=="empty") {
+			if (myStack.empty())
+				cout << 1 << "\n";
+			else
+				cout << 0 << "\n";
+		}
+		else {
+			if (myStack.empty())
+				cout << -1 << "\n";
+			else {
+				cout<<myStack.top() << "\n";
+			}
+
+		}
+		N--;
+	}
+
+
+}
+
