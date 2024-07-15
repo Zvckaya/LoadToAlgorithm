0144b5545ed928fcd2a5cde5cb01f08c56defa4e
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-15 20:45:33 +0900

    no10816

---
+++ no10816/no10816.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	map<int, int> cards;
+
+	int N, M;
+	cin >> N;
+	int tmp;
+	for (int i = 0; i < N; i++) {
+		cin >> tmp;
+		cards[tmp]++;
+	}
+
+	cin >> M;
+	for (int j = 0; j < M; j++) {
+		cin >> tmp;
+		if (cards.find(tmp) != cards.end())
+			cout << cards[tmp] << " ";
+		else
+			cout << 0 <<" ";
+	}
+}
+
