334389de841c0c87834eacac4a1b014eaae41a48
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-15 20:36:11 +0900

    no9012

---
+++ no9012/no9012.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	string str;
+	int vpsCount;
+	bool isVps;
+
+	for (int i = 0; i < N; i++) {
+		vpsCount = 0;
+		isVps = true;
+
+		cin >> str;
+		for (auto i : str) {
+			if (vpsCount < 0)
+				isVps = false;
+
+			if (i == '(') {
+				vpsCount++;
+			}
+			else {
+				vpsCount--;
+			}
+		}
+		if (vpsCount==0 and isVps==true)
+			cout << "YES" << "\n";
+		else
+			cout << "NO" << "\n";
+
+
+	}
+}
+
