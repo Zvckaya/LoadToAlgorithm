76c222e6053edf419a013931c3dfe0181d9b8164
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-18 15:41:56 +0900

    no11050

---
+++ no11050/no11050.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int binomialCoefficient(int n, int k) {
+    return factorial(n) / (factorial(k) * factorial(n - k));
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, K;
+    cin >> N >> K;
+
+    cout << binomialCoefficient(N, K);
+
+    return 0;
+}
