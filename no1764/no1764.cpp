69ba3198e1b1a93624324745371e5d4067e8ab01
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2025-01-05 22:33:14 +0900

    no1764 solve

---
+++ no1764/no1764.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+
+    unordered_set<string> noHear;
+
+    for (int i = 0; i < N; i++) {
+        string name;
+        cin >> name;
+        noHear.insert(name);
+    }
+
+    vector<string> result;
+    result.reserve(M);
+
+    for (int i = 0; i < M; i++) {
+        string name;
+        cin >> name;
+        if (noHear.find(name) != noHear.end()) {
+            result.push_back(name);
+        }
+    }
+
+    sort(result.begin(), result.end());
+
+    cout << result.size() << endl;
+    for (auto& name : result) {
+        cout << name << endl;
+    }
+
+    return 0;
+}
