3a13b81b7a7ae6fada9da33d1de4c70b509763b9
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2025-01-12 21:11:38 +0900

    no17219

---
+++ no17219/no17219.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+
+	unordered_map<string,string> sites;
+	for (int i = 0; i < N; i++) {
+		string site, password;
+		cin >> site >> password;
+		sites[site] = password;
+	}
+	vector<string> answer;
+
+	for (int i = 0; i < M; i++) {
+		string find = "";
+		cin >> find;
+		answer.push_back(sites[find]);
+	}
+
+	for (auto i : answer) {
+		cout << i << endl;
+	}
+
+
+}
+
