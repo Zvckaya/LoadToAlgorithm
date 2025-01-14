74bde4ba01fbd4170aaaf6937db3e5aaf2cc369d
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2025-01-14 20:33:18 +0900

    no 1074 no 9095 solve

---
+++ no1074/no1074.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+
+long long Z(int N, int r, int c) {
+
+    if (N == 0) {
+        return 0;
+    }
+
+    int half = pow(2, N - 1); 
+
+    int blockSize = pow(2, 2*(N-1));
+
+    int idx = 0;
+
+    if (r >= half) {
+        idx += 2;       
+        r -= half;      
+    }
+
+    if (c >= half) {
+        idx += 1;       
+        c -= half;      
+    }
+
+
+    return idx * blockSize + Z(N - 1, r, c);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    int r, c;
+    cin >> N >> r >> c;
+
+    cout << Z(N, r, c) << "\n";
+    return 0;
+}
---
+++ no9095/no9095.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> test;
+	
+	for (int i = 0; i < n; i++) {
+		int temp;
+		cin >> temp;
+		test.push_back(temp);
+	}
+
+	int arr[12] = { 0 };
+
+	arr[0] = 0;
+	arr[1] = 1;
+	arr[2] = 2;
+	arr[3] = 4;
+	for (int i = 4; i <= 11; i++) {
+		arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+	}
+
+	for (auto i : test) {
+		cout << arr[i] << endl;
+	}
+}
+
