d20e2d7016151ebd32070b5f0a77b892b2e62207
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2025-01-15 22:33:49 +0900

    no 10989 no 2751 solve

---
+++ no2751/no2751.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+static const int MAX_VAL = 1000000;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<int> count(MAX_VAL + 1, 0);
+
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        count[x]++;
+    }
+
+    for (int i = 1; i <= MAX_VAL; i++) {
+		if (count[i] == 0) 
+            continue;
+        cout << i << "\n";
+    }
+
+    return 0;
+}
