256b896045ac0437c8ba939c2531ddd1dc31086a
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2025-01-16 22:42:10 +0900

    no9375

---
+++ no9375/no9375.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+
+
+int main()
+{
+
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		int n;
+		cin >> n;
+		unordered_map<string, int> clothes;
+		
+		for (int i = 0; i < n; i++) {
+			string name, type;
+			cin >> name >> type;
+			clothes[type]++;
+		}
+
+		int result = 1;
+
+		for (auto c : clothes)
+		{
+			result *= (c.second + 1);
+		}
+
+		result -= 1;
+
+		cout << result << endl;
+
+	}
+
+}
+
