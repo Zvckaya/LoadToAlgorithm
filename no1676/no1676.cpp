54b78688236a535520640a896d9b802d81c50030
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2025-01-23 19:34:25 +0900

    no1676

---
+++ no1676/no1676.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    int countZero = 0;
+
+    for (long long p = 5; p <= N; p *= 5) {
+        countZero += N / p;
+    }
+
+    cout << countZero << "\n";
+    return 0;
+
+}
