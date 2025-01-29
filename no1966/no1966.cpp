f99497a258c80da6b3115edc56ca04741d5e87aa
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2025-01-29 16:17:29 +0900

    no1966 solve

---
+++ no1966/no1966.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		int N, M;
+		cin >> N >> M;
+		queue<pair<int, int>> q;
+		priority_queue<int> pq;
+		for (int i = 0; i < N; i++)
+		{
+			int x;
+			cin >> x;
+			q.push({ x, i });
+			pq.push(x);
+		}
+		int cnt = 0;
+		while (!q.empty())
+		{
+			int x = q.front().first;
+			int y = q.front().second;
+			q.pop();
+			if (pq.top() == x)
+			{
+				pq.pop();
+				cnt++;
+				if (y == M)
+				{
+					cout << cnt << '\n';
+					break;
+				}
+			}
+			else
+			{
+				q.push({ x, y });
+			}
+		}
+	}
+	return 0;
+
+}
