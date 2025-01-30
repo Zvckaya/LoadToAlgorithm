d6571a5e1101fc4157e0d68737ec7b5df89e7e6a
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2025-01-30 14:15:46 +0900

    no10773 solve

---
+++ no10773/no10773.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <cmath>
+
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int K;
+    cin >> K;
+
+    stack<long long> st;  
+
+    while (K--) {
+        long long x;
+        cin >> x;
+        if (x == 0) {
+            if (!st.empty()) {
+                st.pop();
+            }
+        }
+        else {
+            st.push(x);
+        }
+    }
+
+    long long result = 0;
+    while (!st.empty()) {
+        result += st.top();
+        st.pop();
+    }
+
+    cout << result << "\n";
+    return 0;
+}
