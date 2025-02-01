fabaaccd7e7cb43be8912d5173a5f4f08d99defe
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2025-02-01 21:32:46 +0900

    no4949 solve

---
+++ no4949/no4949.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	string s;
+	while (true) {
+
+		getline(cin, s);
+
+		if (s == ".") {
+			break;
+		}
+		stack<char> st;
+		bool flag = true;
+		for (char c : s) {
+			if (c == '(' || c == '[') {
+				st.push(c);
+			}
+			else if (c == ')') {
+				if (st.empty() || st.top() != '(') {
+					flag = false;
+					break;
+				}
+				st.pop();
+			}
+			else if (c == ']') {
+				if (st.empty() || st.top() != '[') {
+					flag = false;
+					break;
+				}
+				st.pop();
+			}
+		}
+		if (flag && st.empty()) {
+			cout << "yes\n";
+		}
+		else {
+			cout << "no\n";
+		}
+	}
+	return 0;
+}
+
