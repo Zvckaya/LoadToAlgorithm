1b8757f4ec759c1ddbcbe2dac470114b7245ccc8
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2025-02-02 21:14:49 +0900

    no25083 solve

---
+++ no25083/no25083.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string S;
+	cin >> S;
+
+	int i= 0;
+	cin >> i;
+
+	cout << S[i - 1] << '\n';
+
+
+
+	
+
+}
