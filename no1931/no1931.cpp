0a9c5ad226ed37982e31ad00a4552f1698f0a4c1
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2025-02-04 22:37:48 +0900

    no28702 solve

---
+++ no1931/no1931.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N;
+	cin >> N;
+	vector<pair<int, int>> v(N);
+	for (int i = 0; i < N; i++) {
+		cin >> v[i].second >> v[i].first;
+	}
+	sort(v.begin(), v.end());
+	int cnt = 1;
+	int end = v[0].first;
+	for (int i = 1; i < N; i++) {
+		if (v[i].second >= end) {
+			cnt++;
+			end = v[i].first;
+		}
+	}
+	cout << cnt << '\n';
+	return 0;
+}
+
