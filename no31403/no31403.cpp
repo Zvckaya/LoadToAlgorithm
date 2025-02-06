7baa084100fe7d7d9161973a455f3a8a09949de1
lzvckaya@gmail.com <lzvckaya@gmail.com>
2025-02-06 22:27:38 +0900

    no31403 solve

---
+++ no31403/no31403.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string A, B, C;
+    cin >> A >> B >> C;
+
+    int aNum = stoi(A);
+    int bNum = stoi(B);
+    int cNum = stoi(C);
+
+    cout << (aNum + bNum) - cNum << "\n";
+
+
+    string AB = A + B;        
+    int abVal = stoi(AB); 
+    int ans = abVal - cNum;
+    cout << ans << "\n";
+
+    return 0;
+}
+
