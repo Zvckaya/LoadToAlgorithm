1ed5a5c81ea99c8ad8ab871941d21ebb58cdc82c
lzvckaya@gmail.com <lzvckaya@gmail.com>
2025-02-17 20:54:05 +0900

    no1541 solve

---
+++ no1541/no1541.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string s;
+	cin >> s;
+
+	vector<int> num;
+	vector <char> op;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == '+' || s[i] == '-')
+		{
+			if (s[i] == '+') {
+				op.push_back('+');
+			}
+			else {
+				op.push_back('-');
+			}
+			continue;
+		}
+		int n = 0;
+		while (i < s.size()&&s[i]!= '+'&&s[i]!='-')
+		{
+			n = n * 10 + (s[i] - '0');
+			i++;
+		}
+		num.push_back(n);
+		i--;
+	}
+
+	int ans = num[0];
+
+	bool minus = false;
+
+	for (int i = 0; i < op.size(); i++) {
+		if (op[i] == '-') {
+			minus = true;
+
+		}
+		if (minus == true) {
+			ans -= num[i + 1];
+
+		}
+		else {
+			ans += num[i + 1];
+		}
+	}
+
+
+	cout << ans << '\n';
+	
+	return 0;
+
+
+}
