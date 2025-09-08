410767f5299e6713b72557e6ecdf8d227bef69c1
lzvckaya <lzvckaya@gmail.com>
2025-09-08 21:40:46 +0900

    프로그래머스12906, 42586

---
+++ 12906/12906.cpp
@@ -0,0 +1,28 @@
+﻿#include <vector>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    vector<int> answer;
+    auto iterator = arr.begin();
+
+    while (iterator != arr.end());
+    {
+        if (answer.empty()) {
+            answer.push_back(*iterator);
+        }
+        if (answer.back() != *iterator) {
+            answer.push_back(*iterator);
+        }
+        iterator++;
+    }
+
+    return answer;
+}
+
+int main() {
+
+}
---
+++ 42586/42586.cpp
@@ -0,0 +1,40 @@
+﻿#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    queue<int> q;
+
+    for (int i = 0; i < (int)progresses.size(); ++i) {
+        int remain = 100 - progresses[i];
+        int days = (remain + speeds[i] - 1) / speeds[i];
+        q.push(days);
+    }
+
+    while (!q.empty()) {
+        int cur = q.front(); 
+        q.pop();
+        int cnt = 1;
+        while (!q.empty() && q.front() <= cur) {
+            q.pop();
+            ++cnt;
+        }
+        answer.push_back(cnt);
+    }
+    return answer;
+}
+
+int main() {
+    auto r1 = solution({ 93, 30, 55 }, { 1, 30, 5 });
+    auto r2 = solution({ 95, 90, 99, 99, 80, 99 }, { 1, 1, 1, 1, 1, 1 });
+
+    for (int x : r1) cout << x << ' '; // 2 1
+    cout << '\n';
+    for (int x : r2) cout << x << ' '; // 1 3 2
+    cout << '\n';
+    return 0;
+}
