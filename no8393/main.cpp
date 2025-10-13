4edba08a315da49e3f29a3e629a362f84e28f80b
lzvckaya <lzvckaya@gmail.com>
2025-10-14 00:35:27 +0900

    no8393 solve

---
+++ no8393/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n, sum = 0;
+	cin >> n;
+	for (int i = 0; i <= n; i++) {
+		sum += i;
+	}
+	cout << sum;
+
+}
