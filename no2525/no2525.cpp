1f216077b9990df8c05018c688eb5fbbb2c7265d
lzvckaya <lzvckaya@gmail.com>
2025-10-14 00:03:06 +0900

    no2525 solve

---
+++ no2525/no2525.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int nh, nm, am, time = 0;
+
+	cin >> nh >> nm >> am;
+	time += nh * 60;
+	time += nm + am;
+
+	
+
+
+	
+	if (time < 60 * 24) {
+		cout << time / 60 << " " << time % 60;
+	}
+	else {
+		time -= 60 * 24;
+		cout << time / 60 << " " << time % 60;
+	}
+
+
+}
