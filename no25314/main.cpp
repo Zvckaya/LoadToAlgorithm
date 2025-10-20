491cc53a3a6d323f620ceaabced8c7157df67656
lzvckaya <lzvckaya@gmail.com>
2025-10-20 12:06:26 +0900

    저장

---
+++ no25314/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	n = n / 4;
+	for (int i = 0; i < n; i++) {
+		cout << "long ";
+	}
+	cout << "int";
+
+	
+
+}
